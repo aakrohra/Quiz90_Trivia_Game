b5488ad9fb077f73d2b8e1ddf326fc94feab0143
Liam <liam.huynh@mail.utoronto.ca>
2024-11-04 16:24:06 -0500

    created Player interface and player entities according to clean architecture mostly. Lots of TODOs that will need to be looked at after the login portion of the program is finished -> need to know how to create Player objects with what data.

---
+++ src/main/java/entity/GuestPlayer.java
@@ -0,0 +1,41 @@
+package entity;
+
+/**
+ * An implementation of the Player interface.
+ * Entity that represents a Guest player.
+ */
+public class GuestPlayer implements Player {
+
+    private final String username;
+    // TODO Change this line depending on implementation of Guest user?
+    //      Idk how it works with login API -> Albert look at probably
+    // private final String password = null;
+    private final int currentScore;
+
+    public GuestPlayer() {
+        this.username = "Guest";
+        this.currentScore = 0;
+    }
+
+    @Override
+    public String getUsername() {
+        return "This is a Guest account";
+    }
+
+    @Override
+    public String getPassword() {
+        return "This is a guest player, it does not have a password.";
+    }
+
+    @Override
+    public String getCurrentScore() {
+        return "The current score of " + this.username + "is " + this.currentScore + ".";
+    }
+
+    @Override
+    public String getHighScore() {
+        return "Guest players do not have a high score.";
+    }
+
+}
+
