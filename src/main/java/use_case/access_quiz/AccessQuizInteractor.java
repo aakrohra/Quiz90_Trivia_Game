8e68d859336e14ad9884139adab64c97ff123853
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2024-11-12 14:03:39 -0500

    more fixes to accessing quiz logic

--- src/main/java/use_case/access_quiz/AccessQuizInteractor.java
+++ src/main/java/use_case/access_quiz/AccessQuizInteractor.java
@@ -8,23 +8,31 @@ import entity.Quiz;
 public class AccessQuizInteractor implements AccessQuizInputBoundary {
 
     private final AccessQuizUserDataAccessInterface customQuizDataAccessObject;
+    private final AccessQuizOutputBoundary accessQuizPresenter;
 
-    public AccessQuizInteractor(AccessQuizUserDataAccessInterface customQuizDataAccessObject) {
+    public AccessQuizInteractor(AccessQuizUserDataAccessInterface customQuizDataAccessObject,
+                                AccessQuizOutputBoundary accessQuizPresenter) {
         this.customQuizDataAccessObject = customQuizDataAccessObject;
+        this.accessQuizPresenter = accessQuizPresenter;
     }
 
     @Override
     public void execute(AccessQuizInputData accessQuizInputData) {
 
+        // why is this an issue?
         final String key = AccessQuizInputData.getKey();
 
         if (customQuizDataAccessObject.existsByKey(key)) {
             final Object customQuiz = customQuizDataAccessObject.getQuizFromKey(key);
+            // do something to turn Object customQuiz into a QuizObject.
+            Quiz quizObject = null;
+            final AccessQuizOutputData accessQuizOutputData = new AccessQuizOutputData(
+                    false, "", 0, quizObject);
+            accessQuizPresenter.prepareSuccessView(accessQuizOutputData);
         }
         else {
-            // fail view in output boundary??
+            accessQuizPresenter.prepareFailView("There is no quiz with the key \"" + key + "\".");
         }
-
     }
 
     @Override
