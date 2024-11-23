78376c9338bd9dd7c2001b0b2c8936f8ac24e66a
Aref-Mal <aref.malekanian@mail.utoronto.ca>
2024-11-23 13:48:52 -0500

    trying to merge

--- src/main/java/use_case/quiz_generation/GenerateQuizInputData.java
+++ src/main/java/use_case/quiz_generation/GenerateQuizInputData.java
@@ -1,4 +1,29 @@
 package use_case.quiz_generation;
 
-public class GenerateQuizInputData {
+/**
+ * The Input Data for the Quiz Generation Use Case.
+ */
+public class QuizGenerationInputData {
+
+    private final String category;
+    private final int numberOfQuestions;
+    private final String difficulty;
+
+    public QuizGenerationInputData(String category, int numberOfQuestions, String difficulty) {
+        this.category = category;
+        this.numberOfQuestions = numberOfQuestions;
+        this.difficulty = difficulty;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public int getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
 }
