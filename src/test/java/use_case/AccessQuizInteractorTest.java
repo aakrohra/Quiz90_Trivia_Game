e71d9bce0328ec81a8920b84f7acf3c21d017e26
Aakaash Rohra <57458928+aakrohra@users.noreply.github.com>
2024-12-03 00:52:55 -0500

    unit testing for access quiz interactor

--- src/test/java/use_case/AccessQuizInteractorTest.java
+++ src/test/java/use_case/AccessQuizInteractorTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import use_case.access_quiz.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class AccessQuizInteractorTest {
 
@@ -25,8 +26,6 @@ public class AccessQuizInteractorTest {
             }
         };
 
-
-
         AccessQuizOutputBoundary accessQuizOB = new AccessQuizOutputBoundary() {
             @Override
             public void prepareSuccessView(AccessQuizOutputData outputData) {
@@ -40,21 +39,140 @@ public class AccessQuizInteractorTest {
 
             @Override
             public void prepareFailView(String error) {
-
+                fail("Use case failure not expected.");
             }
 
             @Override
             public void switchToLoggedInView() {
-
+                fail("Not expected.");
             }
 
             @Override
             public void playAccessedQuiz(RetrievedQuiz quizObject) {
-
+                fail("Not expected.");
             }
         };
 
         AccessQuizInteractor accessQuizInteractor = new AccessQuizInteractor(customQuizDAO, accessQuizOB);
         accessQuizInteractor.execute(new AccessQuizInputData("0123456789aak"));
+    }
+
+    @Test
+    public void testExecuteAccessQuizFromKeyFailure() {
+        AccessQuizDataAccessInterface customQuizDAO = new AccessQuizDataAccessInterface() {
+            @Override
+            public boolean existsByKey(String key) {
+                return false;
+            }
+
+            @Override
+            public JSONObject getQuizFromKey(String key) {
+                return null;
+            }
+        };
+        AccessQuizOutputBoundary accessQuizOB = new AccessQuizOutputBoundary() {
+            @Override
+            public void prepareSuccessView(AccessQuizOutputData outputData) {
+                fail("Use case success not expected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("There is no quiz with the key \"0123456789aak\".", error);
+            }
+
+            @Override
+            public void switchToLoggedInView() {
+                fail("Not expected.");
+            }
+
+            @Override
+            public void playAccessedQuiz(RetrievedQuiz quizObject) {
+                fail("Not expected.");
+            }
+        };
+
+        AccessQuizInteractor accessQuizInteractor = new AccessQuizInteractor(customQuizDAO, accessQuizOB);
+        accessQuizInteractor.execute(new AccessQuizInputData("0123456789aak"));
+    }
+
+    @Test
+    public void testSwitchToLoggedInView() {
+        AccessQuizDataAccessInterface customQuizDAO = new AccessQuizDataAccessInterface() {
+            @Override
+            public boolean existsByKey(String key) {
+                return false;
+            }
+
+            @Override
+            public JSONObject getQuizFromKey(String key) {
+                return null;
+            }
+        };
+        AccessQuizOutputBoundary accessQuizOB = new AccessQuizOutputBoundary() {
+            @Override
+            public void prepareSuccessView(AccessQuizOutputData outputData) {
+                fail("Use case success not expected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case failure not expected.");
+            }
+
+            @Override
+            public void switchToLoggedInView() {
+                // only needs to exist to ensure no error when calling, no code necessary.
+            }
+
+            @Override
+            public void playAccessedQuiz(RetrievedQuiz quizObject) {
+                fail("Not expected.");
+            }
+        };
+
+        AccessQuizInteractor accessQuizInteractor = new AccessQuizInteractor(customQuizDAO, accessQuizOB);
+        accessQuizInteractor.switchToLoggedInView();
+    }
+
+    @Test
+    public void testPlayAccessedQuiz() {
+        AccessQuizDataAccessInterface customQuizDAO = new AccessQuizDataAccessInterface() {
+            @Override
+            public boolean existsByKey(String key) {
+                return false;
+            }
+
+            @Override
+            public JSONObject getQuizFromKey(String key) {
+                return null;
+            }
+        };
+        AccessQuizOutputBoundary accessQuizOB = new AccessQuizOutputBoundary() {
+            @Override
+            public void prepareSuccessView(AccessQuizOutputData outputData) {
+                fail("Use case success not expected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case failure not expected.");
+            }
+
+            @Override
+            public void switchToLoggedInView() {
+                fail("Not expected.");
+            }
+
+            @Override
+            public void playAccessedQuiz(RetrievedQuiz quizObject) {
+                // only need to make sure this is called correctly without error - another test already covers...
+                // ...making sure the quizObject in accurate (success view test).
+            }
+        };
+        RetrievedQuizFactory quizFac = new RetrievedQuizFactory();
+        RetrievedQuiz quizObject = quizFac.create(new JSONObject("{\"title\": \"titleTest\", \"questions\": [{\"questionText\": \"questionText1\", \"options\": [\"o1\", \"o2\", \"o3\", \"o4\"], \"correct\": \"o2\"}, {\"questionText\": \"question222!!!\", \"options\": [\"oo1\", \"oo2\", \"oo3\", \"oo4\"], \"correct\": \"oo4\"}]}"), "0123456789aak");
+        AccessQuizInteractor accessQuizInteractor = new AccessQuizInteractor(customQuizDAO, accessQuizOB);
+        accessQuizInteractor.playAccessedQuiz(quizObject);
     }
 }
